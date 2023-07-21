1eb09b8cb327c1d5c620e46d601b114e563821b9
Teodora <teamarkovic18@gmail.com>
2023-07-21 23:02:24 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var r = prompt("Unesite prečnik valjka:");
+var h = prompt("Unesite visinu valjka:");
+const pi = 3.14;
+var zapremina = parseInt(r)*parseInt(r)*parseInt(h)*pi;
+alert("Površina valjka je:"+ zapremina);
---
+++ script1.js
@@ -0,0 +1,3 @@
+let r = parseFloat(prompt("Please enter radius:", 0));
+let p = Math.pow(r, 2) * Math.PI;
+alert("The surface of the circle is " + p.toFixed(2));
